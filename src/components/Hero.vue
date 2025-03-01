b2b92728305320e43f513467b803e1d91ca17082
magnus.coder <kelimbth@gmail.com>
2025-03-01 21:15:37 +0300

    created a Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { defineProps } from 'vue'
+
+    defineProps({
+        title: {
+            type:    String,
+            default: 'Be a Vue dev',
+        },
+        subtitle: {
+            type:    String,
+            default: 'Find the Vue job that fits your skills and needs',
+        },
+    })
+</script>
+
+<template>
+    <section class="bg-success py-5 mb-4 text-center">
+  <div class="container">
+    <h1 class="display-4 fw-bold text-white">{{ title }}</h1>
+    <p class="lead text-white">{{ subtitle }}</p>
+  </div>
+</section>
+</template>
