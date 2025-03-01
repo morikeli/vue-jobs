704bf6817b6ea56c3471abbaa1cccc722b5925ea
magnus.coder <kelimbth@gmail.com>
2025-03-01 21:12:19 +0300

    router config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import './assets/main.css'
+import 'primeicons/primeicons.css'
+import router from './router'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
