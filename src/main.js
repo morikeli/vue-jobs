9e787cbfb154d530805fab60917d6c4174c85a39
magnus.coder <kelimbth@gmail.com>
2025-03-03 10:40:41 +0300

    updated app to show toast notifications

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import './assets/main.css'
 import 'primeicons/primeicons.css'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import router from './router'
 
 import { createApp } from 'vue'
@@ -9,4 +11,5 @@ import App from './App.vue'
 
 const app = createApp(App)
 app.use(router)
+app.use(Toast)
 app.mount('#app')
