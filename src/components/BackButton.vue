a1588b96430fda2d766cf7f45aa3c8b372a69ecc
magnus.coder <kelimbth@gmail.com>
2025-03-03 10:41:03 +0300

    created a button to allow users to go back to job listings page

---
+++ src/components/BackButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <section>
+        <div class="container py-3 px-3">
+            <RouterLink to="/jobs" class="text-success d-flex align-items-center text-decoration-none">
+            <i class="pi pi-arrow-circle-left me-3"></i> Back to Job Listings
+            </RouterLink>
+        </div>
+    </section>
+</template>
